bbd32023c11d0f589e29d71e83c53f5aeff7122b
Samuel Barham <barhamsamuel@gmail.com>
2021-01-11 16:33:40 -0500

    More sophisticated training script

--- corpus.py
+++ corpus.py
@@ -1,3 +1,11 @@
+##
+# TODO
+#    * introduce padding (especially in iter_batches())
+#
+##
+
+
+
 #############
 ## Imports ##
 #############
@@ -12,6 +20,8 @@ import pickle
 import math
 import pathlib
 
+from collections import Counter, defaultdict
+
 import torch
 
 
@@ -19,10 +29,11 @@ import torch
 ## Important Globals ##
 #######################
 
-N = -1 # 1000
+N = 1000
 
 nlp = spacy.load('en_core_web_sm')
 
+unk_tok = '<UNK>'
 num_tok = '<NUM>'
 pad_tok = '<PAD>'
 
@@ -39,7 +50,7 @@ random.seed(777)
 #######################
 
 class Dataset:
-    def __init__(self, strings, train_test_split=.8):
+    def __init__(self, strings, train_test_split=.8, min_freq=1):
         """
         strings : a list of sentences, or lines, or paragraphs -- strings of characters longer than a word
         """
@@ -51,6 +62,7 @@ class Dataset:
         print("Preprocessing the corpus ...")
         self._preprocessed = [self._preprocess(sent) for sent in tqdm.tqdm(self._sentences)]
         
+        print("Creating dataset partitions ...")
         self._words = [word for sent in self._preprocessed for word in sent.split()]
         
         self._train_words = [word for sent in self._preprocessed[:split_idx] for word in sent.split()]
@@ -62,12 +74,19 @@ class Dataset:
             'test': self._test_words
         }
 
-        
-        print("Extracting vocabulary ...")
-        print("   [Phase 1 of 2]")
-        self._i2w = list({word for word in tqdm.tqdm(self._words)})
-        print("   [Phase 2 of 2]")
-        self._w2i = {word: idx for idx, word in tqdm.tqdm(enumerate(self._i2w))}
+        if min_freq > 1:
+            print("Collecting word type counts ...")
+            self._word_freq = Counter(self._words)
+            
+            print("Filtering out low frequency words ...")
+            self._i2w = [unk_tok] + [word for word, freq in self._word_freq.items() if freq >= min_freq]
+        else:
+            print("Extracting vocabulary ...")
+            self._i2w = [unk_tok] + list({word for word in tqdm.tqdm(self._words)})
+            
+        print("Constructing word-to-index mapping ...")
+        self._w2i = defaultdict(int)
+        self._w2i.update({word: idx for idx, word in tqdm.tqdm(enumerate(self._i2w), total=len(self._i2w))})
 
         # record the size of the vocabulary
         self.vocab_size = len(self._i2w)
@@ -76,20 +95,26 @@ class Dataset:
         
     def _preprocess(self, sent):
         sent = nlp(sent)
-        sent = [num_tok if tok.is_digit else tok.text.lower() for tok in sent if not tok.is_punct]
+        sent = [num_tok if (tok.is_digit or tok.like_num or tok.pos_ == 'NUM') else tok.text.lower() \
+                for tok in sent \
+                if not (tok.is_punct or tok.text == '`')]
         
         return ' '.join(sent)
 
-    # API
-    
-    def idx2example(self, idx):
-        return self._preprocessed[idx]
 
+    #####################
+    ## Vocab Utilities ##
+    #####################
+    
     def i2w(self, idx):
         return self._i2w[idx]
 
     def w2i(self, word):
         return self._w2i[word]
+    
+    ######################
+    ## Sample Utilities ##
+    ######################
 
     def sample2idx(self, sample):
         """
@@ -118,17 +143,62 @@ class Dataset:
 
         return sample
 
-    def get_batch(self, sample_length, batch_size):
-        batch = [self.get_sample(sample_length + 1) for _ in range(batch_size)]
-        xs, ys = list(zip(*[(sample[:-1], sample[-1]) for sample in batch]))
+    ########################
+    ## Batching Utilities ##
+    ########################
+    
+    def samples2input_label(self, samples):
+        return list(zip(*[(sample[:-1], sample[-1]) for sample in samples]))
 
-        return torch.LongTensor(xs), torch.LongTensor(ys)
+    def batch2tensor(self, batch):
+        return torch.LongTensor(batch[0]), torch.LongTensor(batch[1])
+    
+    #########################################
+    ## Iterate Through a Dataset Partition ##
+    ##    in order -- one by one           ##  
+    #########################################
+    
+    def get_num_batches(self, sample_length, batch_size, partition='train'):
+        words_in_batch = sample_length * batch_size
+        data = self._partitions[partition]
+        
+        return math.floor(len(data) / words_in_batch)
+    
+    def get_next_batch(self, idx, sample_length, batch_size, partition='train'):
+        partition = self._partitions[partition]
+
+        start_idx = idx * sample_length * batch_size
+
+        samples = [partition[start_idx + (batch * sample_length) : start_idx + ((batch + 1) * sample_length)] \
+                for batch in range(batch_size)]
+
+        samples = [self.sample2idx(sample) for sample in samples]
+        
+        return self.batch2tensor(self.samples2input_label(samples))
+
+    
+    def iter_batch(self, sample_length, batch_size, partition='train'):
+        num_batches = self.get_num_batches(sample_length + 1, batch_size, partition)
+        
+        return (self.get_next_batch(idx, sample_length + 1, batch_size, partition) for idx in range(num_batches))
+
+    #############################################
+    ## Sample Batches From a Dataset Partition ##
+    ##    each sample in the batch drawn at    ##
+    ##    at random                            ##
+    #############################################
+    
+    def get_batch(self, sample_length, batch_size, partition='train'):
+        samples = [self.get_sample(sample_length + 1, partition) for _ in range(batch_size)]
+        
+        return self.batch2tensor(self.samples2input_label(samples))
+
     
 
 
 # Example Usage: Representing the Brown Corpus
     
-brown_sentences = [' '.join(sent) for sent in tqdm.tqdm(nltk.corpus.brown.sents())][:N]
+brown_sentences = [' '.join(sent) for sent in tqdm.tqdm(nltk.corpus.brown.sents())]
 
 dataset_path = pathlib.Path('dataset.pkl')
 
@@ -139,6 +209,6 @@ if dataset_path.exists():
     with open(dataset_path, 'rb') as f:
         brown_dataset = pickle.load(f)
 else:
-    brown_dataset = Dataset(brown_sentences)
+    brown_dataset = Dataset(brown_sentences, min_freq=3)
     with open(dataset_path, 'wb') as f:
         pickle.dump(brown_dataset, f)
--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import math
+from pathlib import Path
+
+import tqdm
 
 import corpus
 import model
@@ -9,23 +12,49 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 
+from torch.utils.tensorboard import SummaryWriter
+
 # Globals
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 loss = nn.NLLLoss()
 
 # Training Parameters
-print_every = 100
-num_iterations = int(1e5)
+print_every = 500
+test_every = 2000
+adjust_every = 500
+num_iterations = int(1e7)
+epsilon_0 = 1e-3
+r = 1e-8
 
 # Hyperparameters
 sample_length = 5 # 100
-batch_size = 64
+batch_size = 512
 
 # Model Parameters
 emb_dim = 60
 hidden_dim = 50
 
+# Tensorboard-related
+experiment_name = Path('act-{}_emb-{}_hidden-{}_sample-{}_batch-{}_optim-{}_lr-{}_adjust-every-{}'.format(
+    'relu',
+    emb_dim,
+    hidden_dim,
+    sample_length,
+    batch_size,
+    'sgd',
+    epsilon_0,
+    adjust_every
+))
+experiment_path = Path('run') / experiment_name
+writer = SummaryWriter(experiment_path)
+
 # Get the dataset
 dataset = corpus.brown_dataset
+num_test_batches = dataset.get_num_batches(
+    sample_length,
+    batch_size,
+    partition='test'
+)
 
 # Create the model
 print("Initializing model ...")
@@ -35,18 +64,19 @@ lm = model.ProbabilisticNeuralLM(
     hidden_dim=hidden_dim,
     sample_length=sample_length
 )
-
-optimizer = optim.Adam(lm.parameters(), amsgrad=True)
+lm.to(device)
+optimizer = optim.SGD(lm.parameters(), lr=epsilon_0) # optim.Adam(lm.parameters(), amsgrad=True)
 
 losses = []
 
 print("Beginning training ...")
-for i in range(num_iterations):
+for i in range(num_iterations):    
     # Zero the parameter gradients
     optimizer.zero_grad()
     
     # Get the next batch
     xs, ys = dataset.get_batch(sample_length, batch_size)
+    xs, ys = xs.to(device), ys.to(device)
 
     # Get the averaged batch loss
     output = loss(lm(xs), ys)
@@ -68,4 +98,42 @@ for i in range(num_iterations):
         # Compute perplexity
         perp = math.exp(avg_loss)
 
+        # Print and log the train loss
         print("({}) : {}".format((i + 1), perp))
+        writer.add_scalar('Training Loss', perp, i)
+
+    if i % test_every == (test_every - 1):
+        print("\nEvaluating model on test set ...")
+        
+        lm.eval()
+
+        with torch.no_grad():
+            batches = dataset.iter_batch(
+                sample_length,
+                batch_size,
+                partition='test'
+            )
+
+            test_losses = []            
+            for xs, ys in tqdm.tqdm(batches, total=num_test_batches):
+                xs, ys = xs.to(device), ys.to(device)
+                output = loss(lm(xs), ys)
+                test_losses.append(output.item())
+
+
+        avg_test_loss = sum(test_losses) / len(test_losses)
+        test_losses = []
+
+        # Compute perplexity
+        test_perp = math.exp(avg_test_loss)
+
+        # Print and log the test loss
+        print("Average Test Loss: {}\n".format(test_perp))
+        writer.add_scalar('Test Loss', test_perp, i)
+                
+        lm.train()
+
+    # Update the learning rate every iteration, just as in the paper
+    if i % adjust_every == (adjust_every - 1):
+        for g in optimizer.param_groups:
+            g['lr'] = epsilon_0 / (1 + r * (i / adjust_every))
